633cbc33c45eca9faeba27d8359d6d425fbd6321
Bernardo-Menon <bemenon97@gmail.com>
2022-05-14 10:59:25 -0300

    Exercicios

---
+++ Lista_2/EXERCICIO_1.py
@@ -0,0 +1,6 @@
+# Lista 2 - Desvios Condicionais
+# Exercício 1 - Efetuar a leitura de uma nota e, se o valor for maior ou igual a 60, imprimir na tela "APROVADO"
+
+nota = int(input("Digite uma nota: "))
+if nota >= 60:
+    print("APROVADO")
---
+++ Lista_2/EXERCICIO_2.py
@@ -0,0 +1,8 @@
+# Lista 2 - Desvios Condicionais
+# Exercício 2 - Efetuar a leitura de uma nota e, se o valor for maior ou igual a 60, imprimir na tela "APROVADO", se for menor, imprimir "REPROVADO".
+
+nota = int(input("Digite uma nota: "))
+if nota >= 60:
+    print("APROVADO")
+else:
+    print ("REPROVADO")
